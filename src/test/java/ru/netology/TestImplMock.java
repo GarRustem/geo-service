6cfa64de1a4b41d030e8717efdbbf2b4cde933df
Rustem Garifullin <95448415+GarRustem@users.noreply.github.com>
2023-01-08 19:26:52 +0500

    Update TestImplMock.java

--- src/test/java/ru/netology/TestImplMock.java
+++ src/test/java/ru/netology/TestImplMock.java
@@ -27,8 +27,8 @@ public class TestImplMock {
 
     @BeforeEach
     void beforeEach() {
-        geoService = Mockito.mock(GeoServiceImpl.class);
-        locService = Mockito.mock(LocalizationServiceImpl.class);
+        geoService = Mockito.spy(GeoServiceImpl.class);
+        locService = Mockito.spy(LocalizationServiceImpl.class);
         msImpl = new MessageSenderImpl(geoService, locService);
         headers = new HashMap<String, String>();
     }
@@ -38,7 +38,7 @@ public class TestImplMock {
         //Test Data
         String ip = "172.123.12.19";
         String testData = "Zdravstvuite";
-        Location locationRU = new Location("Moscow", Country.RUSSIA, "Square", 11);
+        Location locationRU = geoService.byIp(ip);
         //Condition
         when(geoService.byIp(ip)).thenReturn(locationRU);
         when(locService.locale(Country.RUSSIA)).thenReturn(testData);
@@ -53,7 +53,7 @@ public class TestImplMock {
         //Test Data
         String ip = "96.123.12.19";
         String testData = "Welcome";
-        Location locationEN = new Location("New York", Country.USA, "Avenue", 9);
+        Location locationEN = geoService.byIp(ip);
         //Condition
         when(geoService.byIp(ip)).thenReturn(locationEN);
         when(locService.locale(Country.USA)).thenReturn(testData);
@@ -66,17 +66,18 @@ public class TestImplMock {
     @Test
     public void testPositiveThreeLocationFromIp() {
         // Test Data
-        Location location = new Location("New York", Country.USA, " 10th Avenue", 32);
+        String ip = "96.44.183.149";
+        Location location = geoService.byIp(ip);
         String expected = "New York";
         // Condition
-        when(geoService.byIp("96.44.183.149")).thenReturn(location);
+        when(geoService.byIp(ip)).thenReturn(location);
         // Check
         Assertions.assertEquals(expected, location.getCity());
         // Condition Log
         System.out.println(location.getCity());
     }
 
-    @Test
+        @Test
     public void testPositiveCountry() {
         // Test Data
         String expected = "Zdravstvuite";
@@ -87,4 +88,69 @@ public class TestImplMock {
         // Condition Log
         System.out.println(locService.locale(Country.RUSSIA));
     }
+
+    // Варианты для использования .mock() вместо .spy()
+
+//    @BeforeEach
+//    void beforeEach() {
+//        geoService = Mockito.mock(GeoServiceImpl.class);
+//        locService = Mockito.mock(LocalizationServiceImpl.class);
+//        msImpl = new MessageSenderImpl(geoService, locService);
+//        headers = new HashMap<String, String>();
+//    }
+//
+//    @Test
+//    public void testPositiveOneRU() {
+//        //Test Data
+//        String ip = "172.123.12.19";
+//        String testData = "Zdravstvuite";
+//        Location locationRU = new Location("Moscow", Country.RUSSIA, "Square", 11);
+//        //Condition
+//        when(geoService.byIp(ip)).thenReturn(locationRU);
+//        when(locService.locale(Country.RUSSIA)).thenReturn(testData);
+//        //Check
+//        Assertions.assertEquals(testData, locService.locale(Country.RUSSIA));
+//        //Condition Log
+//        System.out.println(locService.locale(Country.RUSSIA));
+//    }
+//
+//    @Test
+//    public void testPositiveTwoEN() {
+//        //Test Data
+//        String ip = "96.123.12.19";
+//        String testData = "Welcome";
+//        Location locationEN = new Location("New York", Country.USA, "Avenue", 9);
+//        //Condition
+//        when(geoService.byIp(ip)).thenReturn(locationEN);
+//        when(locService.locale(Country.USA)).thenReturn(testData);
+//        //Check
+//        Assertions.assertEquals(testData, locService.locale(Country.USA));
+//        //Condition Log
+//        System.out.println(locService.locale(Country.USA));
+//    }
+//
+//    @Test
+//    public void testPositiveThreeLocationFromIp() {
+//        // Test Data
+//        Location location = new Location("New York", Country.USA, " 10th Avenue", 32);
+//        String expected = "New York";
+//        // Condition
+//        when(geoService.byIp("96.44.183.149")).thenReturn(location);
+//        // Check
+//        Assertions.assertEquals(expected, location.getCity());
+//        // Condition Log
+//        System.out.println(location.getCity());
+//    }
+//
+//    @Test
+//    public void testPositiveCountry() {
+//        // Test Data
+//        String expected = "Zdravstvuite";
+//        // Condition
+//        when(locService.locale(Country.RUSSIA)).thenReturn(expected);
+//        // Check
+//        Assertions.assertEquals(expected, locService.locale(Country.RUSSIA));
+//        // Condition Log
+//        System.out.println(locService.locale(Country.RUSSIA));
+//    }
 }
